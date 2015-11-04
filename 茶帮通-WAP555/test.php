4f9a60ac53a58816e0539bf62740ec6ef7b23bdf
zhoucongmin <1641626308@qq.com>
2015-11-04 14:51:09 +0800

    更新

--- 茶帮通-WAP555/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-	sleep(3);
-	$array = array("name" => "kc","status" => 1);
-	//echo json_encode($array);
-	echo '<img src="images/p7.jpg"><img src="images/p6.jpg"><img src="images/p5.jpg"><img src="images/p8.jpg">';
-?>
