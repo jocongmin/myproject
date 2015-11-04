6ea553b73423d4e2b68e8d8ffa0d927e53d41c8c
zhoucongmin <1641626308@qq.com>
2015-11-04 14:39:27 +0800

    wap端

---
+++ 茶帮通-WAP-静态页面/test.php
@@ -0,0 +1,6 @@
+<?php
+	sleep(3);
+	$array = array("name" => "kc","status" => 1);
+	//echo json_encode($array);
+	echo '<img src="images/p7.jpg"><img src="images/p6.jpg"><img src="images/p5.jpg"><img src="images/p8.jpg">';
+?>
