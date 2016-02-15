44ca12bda8ee5319e6036cb068791378660380eb
zhoucongmin <1641626308@qq.com>
2016-02-15 17:15:03 +0800

    new

--- 茶邦通所有页面/js/common.js
+++
@@ -1,8 +0,0 @@
-//替换样式
-$(document).ready(function(){
-	$(".mailTab li").hover(function(){
-		$(this).addClass("checked").siblings().removeClass("checked");
-		$(this).closest("div").find(".mailTablePlan").eq($(this).index()).show().siblings(".mailTablePlan").hide();
-	})
-});
-
